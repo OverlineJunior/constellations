a2f027fe5f7112f868bea26b244b235576d1376c
OverlineJunior <franciscosebastianyjunior@hotmail.com>
2022-08-27 18:37:26 -0300

    remove beam logic that was coupled to the star class.

--- src/shared/star.ts
+++ src/shared/star.ts
@@ -1,6 +1,4 @@
-import { Workspace, RunService } from "@rbxts/services";
-
-const MAX_LINK_DIST = 8;
+import { Workspace } from "@rbxts/services";
 
 interface StarInstance extends Part {
 	Attachment: Attachment;
@@ -47,51 +45,15 @@ function starInstance(pos: Vector3): StarInstance {
 export class Star {
 	readonly instance: StarInstance;
 	readonly links: Array<Star>;
-	private readonly connection: RBXScriptConnection;
 
 	constructor(pos: Vector3) {
 		this.instance = starInstance(pos);
 		this.links = new Array<Star>();
 		stars.set(this.instance, this);
-
-		this.connection = RunService.Heartbeat.Connect(() => {
-			this.handleLink();
-		});
-	}
-
-	private handleLink() {
-		container.GetChildren().forEach((inst) => {
-			const otherStar = stars.get(inst as Part)!;
-
-			if (inst === this.instance || otherStar.links.includes(this)) return;
-
-			const dist = (inst as Part).Position.sub(this.instance.Position).Magnitude;
-			const beam = this.instance.Beam;
-
-			if (dist > MAX_LINK_DIST) {
-				this.links.remove(this.links.indexOf(otherStar));
-				beam.Enabled = false;
-			} else {
-				if (!this.links.includes(otherStar)) {
-					this.links.push(otherStar);
-				}
-
-				const closeMult = math.clamp((MAX_LINK_DIST - dist) / MAX_LINK_DIST, 0, 1);
-				const farMult = 1 - closeMult;
-				const width = closeMult * this.instance.Size.X;
-
-				beam.Enabled = true;
-				beam.Attachment1 = otherStar.instance.Attachment;
-				beam.Transparency = new NumberSequence(farMult);
-				beam.Width0 = width;
-				beam.Width1 = width;
-			}
-		});
 	}
 
 	public destroy() {
 		stars.delete(this.instance);
 		this.instance.Destroy();
-		this.connection.Disconnect();
 	}
 }
